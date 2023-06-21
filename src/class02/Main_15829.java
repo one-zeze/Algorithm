8f409c540fff227d6c0f33575c17f510a2547b2b
one-zeze <tales0708@gmail.com>
2023-06-21 20:40:37 +0900

    15829 modified

--- src/class02/Main_15829.java
+++ src/class02/Main_15829.java
@@ -9,44 +9,37 @@ import java.util.Map;
 public class Main_15829 {
     public static void main(String[] args) throws IOException {
 
-        int r = 31;
-        int M = 1234567891;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int len = Integer.parseInt(br.readLine());
-        var input = br.readLine().split("");
+        var len = Integer.parseInt(br.readLine());
+        var input = br.readLine();
+        int M = 1234567891;
+        int s = 31;
 
-        getH(input, len, r, M);
-    }
-
-    public static void getH(String[] input,int len, int r, int M){
-        int H = 0;
-        int inputSum = 0;
-
+        //a~z 번호부여
         Map<Character, Integer> map = new HashMap<>();
-        int cnt = 1;
+        int cnt=1;
         for (char i='a'; i<='z'; i++){
             map.put(i, cnt);
             cnt++;
         }
-
         System.out.println(map.entrySet());
-        var key = map.keySet();
-        for (var item:key){
-            var value = map.get(item);
-            System.out.println(item+", "+value);
-            inputSum += value * (int) Math.pow(r, value);
-            System.out.print(inputSum+" ");
-        }
-        H = inputSum/M;
-        System.out.println(inputSum+","+(inputSum/M));
 
-//        for (int i=0; i<(len-1); i++){
-//            var value = map.getOrDefault(input[i], 0);
-//            int num = (int) Math.pow(r, value);
-//            inputSum += value*num;
-//        }
-//
-//        H = inputSum/M;
-//        System.out.println(H);
+        int sum=0;
+        for (int i=0; i<len; i++){
+
+            int value = map.get(input.charAt(i));
+            System.out.print(value+" x ");
+
+            int r = 1;
+            for (int j=0; j<i; j++){
+                s %= M;
+                r *= s;
+
+            }
+            System.out.println(r);
+            sum += (value % M) * (r % M);
+        }
+        sum %= M;
+        System.out.println(sum);
     }
 }
