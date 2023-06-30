0ca3aca3c5c1afe6e0be36274a646fefe5b747bc
one-zeze <tales0708@gmail.com>
2023-06-30 21:29:32 +0900

    not solved

---
+++ src/class02/Main_1654.java
@@ -0,0 +1,36 @@
+package class02;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main_1654 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        var input = br.readLine().split(" ");
+        int K = Integer.parseInt(input[0]);
+        int N = Integer.parseInt(input[1]);
+        int[] lenList = new int[K];
+        for (int i=0; i<K; i++){
+            Long length = Long.parseLong(br.readLine());
+            lenList[i] = Math.toIntExact(length);
+        }
+
+        while (true){
+            int newLenght = Arrays.stream(lenList).max().getAsInt();
+
+            for (int i=newLenght; i>0; i--){
+                int cnt=0;
+                for (var index:lenList){
+                    cnt += index/i;
+                }
+                if (cnt >= N){
+                    System.out.println(i);
+                    return;
+                }
+            }
+        }
+    }
+}
