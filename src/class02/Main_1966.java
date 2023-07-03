ca7110466f3bab4cfeaaa95fd054cb7540df275b
one-zeze <tales0708@gmail.com>
2023-07-03 20:52:07 +0900

    not solved

---
+++ src/class02/Main_1966.java
@@ -0,0 +1,26 @@
+package class02;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_1966 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int roop = Integer.parseInt(br.readLine());
+
+        for (int i=0; i<roop; i++){
+            var NM = br.readLine().split(" ");
+            int N = Integer.parseInt(NM[0]);
+            int M = Integer.parseInt(NM[1]);
+
+            var Importance = br.readLine().split(" ");
+        }
+    }
+
+    private static void getSeq(int num, int seq, String[] importance){
+
+        
+    }
+}
