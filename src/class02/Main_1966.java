f10c66c0fa202b17c105fe5f9d89e77fc2d005b6
one-zeze <tales0708@gmail.com>
2023-07-05 19:27:49 +0900

    BaekJoon 1966

--- src/class02/Main_1966.java
+++ src/class02/Main_1966.java
@@ -1,83 +1,77 @@
 package class02;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.io.IOException;
 
+import java.util.StringTokenizer;
+import java.util.LinkedList;
+// solve 출처 : https://st-lab.tistory.com/201
 public class Main_1966 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int roop = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
-        for (int i=0; i<roop; i++){
-            var NM = br.readLine().split(" ");
-            int N = Integer.parseInt(NM[0]);
-            int M = Integer.parseInt(NM[1]);
+        int T = Integer.parseInt(br.readLine());	// 테스트 케이스
 
-            var Importance = br.readLine().split(" ");
+        while (T-- > 0) {
 
-            //문서 개수가 1개인경우 -> 무조건 첫번째로 인쇄
-            if (N == 1){
-                System.out.println(1);
-            }
-            else {
-                var result = getSeq(N, M, Importance);
-                System.out.println(result);
-            }
-        }
-    }
+            StringTokenizer st = new StringTokenizer(br.readLine());
 
-    private static int getSeq(int num, int seq, String[] importance){
-        //중요도 정수형 배열로 저장
-        int size = importance.length;
-        int[] newList = new int[size];
-        for (int i=0; i<size; i++){
-            newList[i] = Integer.parseInt(importance[i]);
-        }
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
 
-        //입력받은 순서대로 Queue에 저장
-        Queue<Integer> queue = new LinkedList<>();
-        for (var item:newList){
-            queue.add(item);
-        }
-        //원하는 문서의 순서:seq, 중요도:newList[seq]
-        var targetSeq = seq;
-        var targetImp = newList[seq];
+            LinkedList<int[]> q = new LinkedList<>();	// Queue로 활용 할 연결리스트
+            st = new StringTokenizer(br.readLine());
 
-        //queue의 최대값이 원하는 문서와 같아질때 까지
-        int max = 0;
-        int min = 0;
-        int cnt = 1;
-        int add = 0;
-        while (max != targetImp){
-            max = Collections.max(queue);
-            min = Collections.min(queue);
-            if (max == targetImp && min == targetImp){
-                return cnt+targetSeq-1;
+            for (int i = 0; i < N; i++) {
+                // {초기 위치, 중요도}
+                q.offer(new int[] { i, Integer.parseInt(st.nextToken()) });
             }
 
-            int front = queue.peek();
-            //중요도가 높은 순서대로 poll(), cnt++
-            if (front == max){
-                queue.poll();
-                cnt++;
-            }
-            // 중요도가 가장높은 문서를 출력하기 전까지 que맨뒤로 삽입
-            else{
-                queue.poll();
-                queue.add(front);
-                add++;
+            int count = 0;	// 출력 횟수
 
-                if (targetSeq > 0){
-                    targetSeq--;
-                }else{
-                    targetSeq = queue.size()-1;
+            while (!q.isEmpty()) {	// 한 케이스에 대한 반복문
+
+                int[] front = q.poll();	// 가장 첫 원소
+                boolean isMax = true;	// front 원소가 가장 큰 원소인지를 판단하는 변수
+
+                // 큐에 남아있는 원소들과 중요도를 비교
+                for(int i = 0; i < q.size(); i++) {
+
+                    // 처음 뽑은 원소보다 큐에 있는 i번째 원소가 중요도가 클 경우
+                    if(front[1] < q.get(i)[1]) {
+
+                        // 뽑은 원소 및 i 이전의 원소들을 뒤로 보낸다.
+                        q.offer(front);
+                        for(int j = 0; j < i; j++) {
+                            q.offer(q.poll());
+                        }
+
+                        // front원소가 가장 큰 원소가 아니였으므로 false를 하고 탐색을 마침
+                        isMax = false;
+                        break;
+                    }
                 }
-            }
-        }
 
-        return cnt;
+                // front 원소가 가장 큰 원소가 아니였으므로 다음 반복문으로 넘어감
+                if(isMax == false) {
+                    continue;
+                }
+
+                // front 원소가 가장 큰 원소였으므로 해당 원소는 출력해야하는 문서다.
+                count++;
+                if(front[0] == M) {	// 찾고자 하는 문서라면 해당 테스트케이스 종료
+                    break;
+                }
+
+            }
+
+            sb.append(count).append('\n');
+
+        }
+        System.out.println(sb);
     }
+
 }
