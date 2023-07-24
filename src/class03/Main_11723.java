9728590f3f86fd27d4fe28dbbfefe2cdb8855063
one-zeze <tales0708@gmail.com>
2023-07-24 20:48:50 +0900

    BaekJoon 11723

--- src/class03/Main_11723.java
+++ src/class03/Main_11723.java
@@ -1,7 +1,57 @@
 package class03;
 
-public class Main_11723 {
-    public static void main(String[] args){
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
 
+public class Main_11723 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        Set<Integer> set = new HashSet<>();
+
+        int M = Integer.parseInt(br.readLine());
+        while (M-- > 0){
+            String[] input = br.readLine().split(" ");
+            String method = input[0];
+
+            switch (method){
+                case "add":
+                    set.add(Integer.parseInt(input[1]));
+                    break;
+                case "remove":
+                    set.remove(Integer.parseInt(input[1]));
+                    break;
+                case "check":
+                    if (set.contains(Integer.parseInt(input[1]))){
+                        sb.append(1).append("\n");
+                    }
+                    else{
+                        sb.append(0).append("\n");
+                    }
+                    break;
+                case "toggle":
+                    if (set.contains(Integer.parseInt(input[1]))){
+                        set.remove(Integer.parseInt(input[1]));
+                    }
+                    else{
+                        set.add(Integer.parseInt(input[1]));
+                    }
+                    break;
+                case "all":
+                    set.clear();
+                    for (int i=1; i<=20; i++){
+                        set.add(i);
+                    }
+                    break;
+                case "empty":
+                    set.clear();
+                    break;
+            }
+        }
+        System.out.print(sb);
     }
 }
