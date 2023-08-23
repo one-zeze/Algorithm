59baa4ef37d7776b4484c25e80bf8f62cb50825d
one-zeze <tales0708@gmail.com>
2023-08-23 17:50:30 +0900

    not solved

---
+++ src/class03/Main_1260.java
@@ -0,0 +1,21 @@
+package class03;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_1260 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int N = Integer.parseInt(input[0]); //정점 개수
+        int M = Integer.parseInt(input[1]); //간선 개수
+        int V = Integer.parseInt(input[2]); //탐색 시작할 정점
+
+        while (M-->0){
+            String[] node_num = br.readLine().split(" ");
+
+        }
+    }
+}
