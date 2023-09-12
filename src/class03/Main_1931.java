f2c34b23041a6a064bf73d86131bd8c53f3ec423
one-zeze <tales0708@gmail.com>
2023-09-12 23:54:55 +0900

    not solved

---
+++ src/class03/Main_1931.java
@@ -0,0 +1,27 @@
+package class03;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main_1931 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        Map<Integer, Integer> timeMap = new HashMap<>();
+        while (N-->0){
+            String[] input = br.readLine().split(" ");
+            int start = Integer.parseInt(input[0]);
+            int end = Integer.parseInt(input[1]);
+
+            timeMap.put(start, end);
+        }
+        // input end
+        // 회의시작 시간 리스트 변환후 정렬
+        List<Integer> start  = new ArrayList<>(timeMap.keySet());
+        Collections.sort(start);
+        
+    }
+}
