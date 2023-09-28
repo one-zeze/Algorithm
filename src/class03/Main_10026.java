248bfe890e60f30c80e198fb91e5d89a8bab96b7
one-zeze <tales0708@gmail.com>
2023-09-28 23:57:59 +0900

    not solved

---
+++ src/class03/Main_10026.java
@@ -0,0 +1,16 @@
+package class03;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_10026 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        while (N-->0){
+            String input = br.readLine();
+        }
+    }
+}
