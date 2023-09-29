b53aea41a6d84a1134db815971c72ce7a4e455b3
one-zeze <tales0708@gmail.com>
2023-09-30 00:55:16 +0900

    백준 10026번

--- src/class03/Main_10026.java
+++ src/class03/Main_10026.java
@@ -15,39 +15,59 @@ public class Main_10026 {
             this.y = y;
         }
     }
-    private static String[][] colorGrid;
-    private static int[][] normal;
+    private static String[][] colorGrid, rgGrid;
     private static boolean[][] visit;
     private static int N;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        colorGrid = new String[N+1][N+1];
-        visit = new boolean[N+1][N+1];
-        normal = new int[N+1][N+1];
+        colorGrid = new String[N][N];
+        rgGrid = new String[N][N];
+        visit = new boolean[N][N];
 
-        for (int i=1; i<=N; i++){
-            colorGrid[i] = br.readLine().split("");
+        for (int i=0; i<N; i++){
+            String[] input = br.readLine().split("");
+            for (int j=0; j<N; j++){
+                colorGrid[i][j] = input[j];
+                if (input[j].equals("R")){
+                    rgGrid[i][j]="G";
+                }else{
+                    rgGrid[i][j] = input[j];
+                }
+            }
         }
         //input end
-        bfs(new Coor(1, 1));
+        int normal=0;
+        int redGreen=0;
 
-        for (var row:normal){
-            for (var col:row){
-                System.out.print(col+" ");
+        for (int i=0; i<N; i++){
+            for (int j=0; j<N; j++){
+                if (!visit[i][j]){
+                    bfs(new Coor(i,j));
+                    normal++;
+                }
             }
-            System.out.println();
-        }
+        }//normal
+        visit = new boolean[N][N];
+        for (int i=0; i<N; i++){
+            for (int j=0; j<N; j++){
+                if (!visit[i][j]){
+                    rg_bfs(new Coor(i,j));
+                    redGreen++;
+                }
+            }
+        }//redGreen
+
+        System.out.println(normal+" "+redGreen);
     }//main method
 
     private static void bfs(Coor start){
         int[] move_x = {-1,1,0,0};
-        int[] move_y = {0,0,-1,0};
+        int[] move_y = {0,0,-1,1};
         Queue<Coor> queue = new LinkedList<>();
         queue.offer(start);
         visit[start.x][start.y] = true;
-        normal[start.x][start.y] = 1;
 
         while (!queue.isEmpty()){
             Coor crt = queue.poll();
@@ -56,15 +76,38 @@ public class Main_10026 {
             for (int i=0; i<4; i++){
                 int newX = crt.x + move_x[i];
                 int newY = crt.y + move_y[i];
-                if (newX>=1 && newX<=N && newY>=1 && newY<=N){
+                if (newX>=0 && newX<N && newY>=0 && newY<N){
                     String next_color = colorGrid[newX][newY];
 
                     if (!visit[newX][newY] && crt_color.equals(next_color)){
-                        normal[newX][newY] = normal[crt.x][crt.y];
-                        
+                        visit[newX][newY] = true;
+                        queue.offer(new Coor(newX,newY));
                     }
-                    else if (!visit[newX][newY] && !crt_color.equals(next_color)){
-                        normal[newX][newY] = normal[crt.x][crt.y]+1;
+                }
+            }
+        }
+    }
+
+    private static void rg_bfs(Coor start){
+        int[] move_x = {-1,1,0,0};
+        int[] move_y = {0,0,-1,1};
+        Queue<Coor> queue = new LinkedList<>();
+        queue.offer(start);
+        visit[start.x][start.y] = true;
+
+        while (!queue.isEmpty()){
+            Coor crt = queue.poll();
+            String crt_color = rgGrid[crt.x][crt.y];
+
+            for (int i=0; i<4; i++){
+                int newX = crt.x + move_x[i];
+                int newY = crt.y + move_y[i];
+                if (newX>=0 && newX<N && newY>=0 && newY<N){
+                    String next_color = rgGrid[newX][newY];
+
+                    if (!visit[newX][newY] && crt_color.equals(next_color)){
+                        visit[newX][newY] = true;
+                        queue.offer(new Coor(newX,newY));
                     }
                 }
             }
