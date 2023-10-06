cfa2bda8bc65c653a9f373e1a08288b6b62140b7
one-zeze <tales0708@gmail.com>
2023-10-06 19:36:33 +0900

    백준 14500, 테트로미노

--- src/class03/Main_14500.java
+++ src/class03/Main_14500.java
@@ -3,9 +3,9 @@ package class03;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
+//import java.util.LinkedList;
+//import java.util.Queue;
 
 public class Main_14500 {
     private static class Coor{
@@ -21,6 +21,9 @@ public class Main_14500 {
     private static int N, M;
     private static int max = 0;
 
+    static int[] dx = {-1,1,0,0};
+    static int[] dy = {0,0,-1,1};
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
@@ -28,63 +31,94 @@ public class Main_14500 {
         M = Integer.parseInt(st.nextToken()); //col
 
         board = new int[N][M];
-        visit = new boolean[N][N];
+        visit = new boolean[N][M];
+
         for (int row=0; row<N; row++){
             st = new StringTokenizer(br.readLine(), " ");
             for (int col=0; col<M; col++){
                 board[row][col] = Integer.parseInt(st.nextToken());
             }
         }
-
+        //input end
+        //        bfs
+//        for (int i=0; i<N; i++){
+//            for (int j=0; j<M; j++){
+//                solve(new Coor(i, j));
+//            }
+//        }
+//        dfs
         for (int i=0; i<N; i++){
             for (int j=0; j<M; j++){
-                solve(new Coor(i, j));
+                visit[i][j] = true;
+                dfs(new Coor(i, j),board[i][j],1);
+                visit[i][j] = false;
             }
         }
+
         System.out.println(max);
-//        solve(new Coor(2,4));
     }// main method
-    private static void solve(Coor start){
-        // 각 시작점에서 도형별로 값 계산후, 최대값 갱신
-        // -> 블록 회전이랑 대칭은?,,,.,.
-        // row 최대 4칸, col 최대 4칸
-        // 블록 최대값 -> 가로4, 세로4 (회전 포함)
-
-        // 시작점부터 거쳐가는 모든 블록에 붙어있는 블록들의 값을 우선순위 큐에 넣고
-        // 큰값을 위주로 탐색하다가, 같은 값/작은값에 둘러 쌓여있으면 우선순위 큐에 값이랑
-        // 비교해서 더하기?,,
-        // 각도형을 첫번째 블록으로 가정한 경우, 모든 노드 블록별로 탐색가능함
-        Queue<Coor> queue = new LinkedList<>();
-        queue.offer(start);
-        visit[0][0] = true;
-        int[] moveX = {0,1,2,3,0,0,0,0,0,1,0,1,0,0,0,1,0,0,0,-1,0,1,2,2,0,1,2,2,0,0,0,1,
-        0,0,0,-1,0,-1,-2,-2,0,-1,-2,-2,0,0,1,1,0,0,-1,-1,0,1,1,2,0,1,1,2,
-        0,0,-1,-1,0,0,1,1,0,-1,-1,-2,0,-1,-1,-2,0,1,1,2,0,1,1,2,0,0,1,0,0,0,-1,0};
-        int[] moveY = {0,0,0,0,0,1,2,3,0,0,1,1,0,1,2,2,0,1,2,2,0,0,0,1,0,0,0,-1,0,-1,-2,-2,
-        0,-1,-2,-2,0,0,0,1,0,0,0,-1,0,1,1,2,0,1,1,2,0,0,-1,-1,0,0,1,1,
-        0,-1,-1,-2,0,-1,-1,-2,0,0,1,1,0,0,-1,-1,0,0,1,0,0,0,-1,0,0,1,1,2,0,1,1,2};
-
-        while (!queue.isEmpty()){
-            Coor crt = queue.poll();
-
-            for (int i=0; i<moveX.length-4; i+=4){
-                int sum = 0;
-                for (int j=i; j<i+4; j++){
-                    int newX = crt.x + moveX[j];
-                    int newY = crt.y + moveY[j];
-                    if (newX<0 || newX>=N || newY<0 || newY>=M){
-                        sum=0;
-                        break;
-                    }
-                    sum += board[newX][newY];
-                }
-                if (sum>max){ //블록 최대값 갱신
-                    max = sum;
-//                    System.out.println(max);
-                }
-            }
-
+    private static void dfs(Coor start, int sum, int cnt){
+        //최대값 갱신
+        if (cnt == 4){
+            max = Math.max(max, sum);
+            return;
         }
 
+        for (int i=0; i<4; i++){
+            int newRow = start.x + dx[i];
+            int newCol = start.y + dy[i];
+            // 범위 검증
+            if (newRow<0 || newRow>=N || newCol<0 || newCol>=M){
+                continue;
+            }
+
+            if (!visit[newRow][newCol]){
+                // ㅗ 블록 값을 구하기 위해서, 2번째 칸에서 한번더 탐색
+                if (cnt==2){
+                    visit[newRow][newCol] = true;
+                    dfs(start, sum+board[newRow][newCol], cnt+1);
+                }
+                
+                visit[newRow][newCol] = true;
+                dfs(new Coor(newRow, newCol), sum+board[newRow][newCol], cnt+1);
+                visit[newRow][newCol] = false;
+            }
+        }
     }
+
+    //bfs 실패, 백트랙킹 비효율적임
+//    private static void solve(Coor start){
+//        Queue<Coor> queue = new LinkedList<>();
+//        queue.offer(start);
+//        visit[0][0] = true;
+//
+//        int[] moveX = {0,1,2,3,0,0,0,0,0,1,0,1,0,0,0,1,0,0,0,-1,0,1,2,2,0,1,2,2,0,0,0,1,
+//        0,0,0,-1,0,-1,-2,-2,0,-1,-2,-2,0,0,1,1,0,0,-1,-1,0,1,1,2,0,1,1,2,
+//        0,0,-1,-1,0,0,1,1,0,-1,-1,-2,0,-1,-1,-2,0,1,1,2,0,1,1,2,0,0,1,0,0,0,-1,0};
+//        int[] moveY = {0,0,0,0,0,1,2,3,0,0,1,1,0,1,2,2,0,1,2,2,0,0,0,1,0,0,0,-1,0,-1,-2,-2,
+//        0,-1,-2,-2,0,0,0,1,0,0,0,-1,0,1,1,2,0,1,1,2,0,0,-1,-1,0,0,1,1,
+//        0,-1,-1,-2,0,-1,-1,-2,0,0,1,1,0,0,-1,-1,0,0,1,0,0,0,-1,0,0,1,1,2,0,1,1,2};
+//
+//        while (!queue.isEmpty()){
+//            Coor crt = queue.poll();
+//
+//            for (int i=0; i<moveX.length-4; i+=4){
+//                int sum = 0;
+//                for (int j=i; j<i+4; j++){
+//                    int newX = crt.x + moveX[j];
+//                    int newY = crt.y + moveY[j];
+//                    if (newX<0 || newX>=N || newY<0 || newY>=M){
+//                        sum=0;
+//                        break;
+//                    }
+//                    sum += board[newX][newY];
+//                }
+//                if (sum>max){ //블록 최대값 갱신
+//                    max = sum;
+////                    System.out.println(max);
+//                }
+//            }
+//
+//        }
+//    }
 }
