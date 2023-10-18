cc248210a05fc568d087270394e471d6e5402948
one-zeze <tales0708@gmail.com>
2023-10-18 19:05:06 +0900

    백준 9019번

--- src/class03/Main_9019.java
+++ src/class03/Main_9019.java
@@ -3,89 +3,60 @@ package class03;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main_9019 {
-    private static class Register{
-        int n;
-        String command_stack;
-
-        public Register(int n, String command_stack) {
-            this.n = n;
-            this.command_stack = command_stack;
-        }
-    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
 
-        StringBuilder sb = new StringBuilder();
         while (T-->0){
-            String[] input = br.readLine().split(" ");
-            int A = Integer.parseInt(input[0]);
-            int B = Integer.parseInt(input[1]);
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
 
-            if (B==0){
-                for (int i=0; i<(A-B); i++){
-                    sb.append("S");
+            Queue<Integer> queue = new LinkedList<>();
+            boolean[] visit = new boolean[10000];
+            String[] command = new String[10000];
+
+            queue.offer(a);
+            visit[a] = true;
+            Arrays.fill(command, "");
+
+            while (!queue.isEmpty()){
+                int crt = queue.poll();
+
+                int D = (2*crt)%10000;
+                int S = crt==0 ? 9999:crt-1;
+                int L = (crt%1000) * 10 + crt/1000;
+                int R = (crt%10) * 1000 + crt/10;
+
+                if (!visit[D]){
+                    queue.offer(D);
+                    visit[D] = true;
+                    command[D] = command[crt] + "D";
+                }
+                if (!visit[S]){
+                    queue.offer(S);
+                    visit[S] = true;
+                    command[S] = command[crt] + "S";
+                }
+                if (!visit[L]){
+                    queue.offer(L);
+                    visit[L] = true;
+                    command[L] = command[crt] + "L";
+                }
+                if (!visit[R]){
+                    queue.offer(R);
+                    visit[R] = true;
+                    command[R] = command[crt] + "R";
                 }
             }
-            else{
-                sb.append(bfs(A, B)).append("\n");
-            }
+            System.out.println(command[b]);
         }
-        System.out.println(sb);
     }//main
-    private static String bfs(int start, int target){
-        String[] command = {"D", "S", "R", "L"};
-
-        boolean[] visit = new boolean[10000];
-        Queue<Register> queue = new LinkedList<>();
-        queue.offer(new Register(start, ""));
-        visit[start] = true;
-
-        while (!queue.isEmpty()){
-            Register crt = queue.poll();
-            if (crt.n == target){
-                return crt.command_stack;
-            }
-            for (int i=0; i<4; i++){
-                Register next = DSLR(command[i], crt);
-                if (!visit[next.n]){
-                    visit[next.n] = true;
-                    queue.offer(next);
-                }
-            }
-        }
-
-        return "";
-    }
-    private static Register DSLR(String cmd, Register crt){
-        int newN = 0;
-
-        if (cmd.equals("D")){
-            newN = 2*crt.n;
-            if (newN>9999){
-                newN %= 10000;
-            }
-        }
-        if (cmd.equals("S")){
-            newN = crt.n-1;
-            if (newN<=0){
-                newN = 9999;
-            }
-        }
-        if (cmd.equals("L")){
-            int temp = crt.n % 1000;
-            newN = (temp * 10) + (crt.n / 1000);
-        }
-        if (cmd.equals("R")){
-            int temp = crt.n % 10;
-            newN = (temp * 1000) + (crt.n / 10);
-        }
-
-        return new Register(newN, crt.command_stack+cmd);
-    }
 }//Class
