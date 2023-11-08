5e7373c3ed4c7c1894d74f13133dcc30a1672b64
one-zeze <tales0708@gmail.com>
2023-11-08 21:05:56 +0900

    add bottom-up solve

--- src/class04/Main_12865.java
+++ src/class04/Main_12865.java
@@ -8,18 +8,18 @@ import java.util.StringTokenizer;
 public class Main_12865 {
     private static int[] W;
     private static int[] V;
-    private static Integer[][] dp;
+    private static int[][] dp;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
         int N = Integer.parseInt(st.nextToken()); //물건 개수
         int K = Integer.parseInt(st.nextToken()); //최대 무게
-        W = new int[N];
-        V = new int[N];
-        dp = new Integer[N][K+1];
+        W = new int[N+1];
+        V = new int[N+1];
+        dp = new int[N+1][K+1];
 
-        for (int i=0; i<N; i++){
+        for (int i=1; i<=N; i++){
             String[] input = br.readLine().split(" ");
             int w = Integer.parseInt(input[0]); //물건 무게
             int v = Integer.parseInt(input[1]); //가치
@@ -28,24 +28,44 @@ public class Main_12865 {
             V[i] = v;
         }
         //end input
-        System.out.print(fillBag(N-1, K));
+//        System.out.print(fillBag(N-1, K));
+        System.out.print(bottomUp(N, K));
     }
     
-    private static int fillBag(int i, int K){
-        //탑다운으로 고민해보자
-        if (i<0){
-            return 0;
+//    private static int fillBag(int i, int K){
+//        //탑다운으로 고민해보자
+//        if (i<0){
+//            return 0;
+//        }
+//
+//        if (dp[i][K] == null){
+//            if (W[i]>K){
+//                dp[i][K] = fillBag(i-1, K);
+//            }
+//            else{
+//                //이부분 이해못함...ㅅ
+//                dp[i][K] = Math.max(fillBag(i-1, K), fillBag(i-1, K-W[i])+V[i]);
+//            }
+//        }
+//
+//        return dp[i][K];
+//    }
+
+    private static int bottomUp(int N, int K){
+        for (int i=1; i<=K; i++){
+            for (int j=1; j<=N; j++){
+                int weight = W[j];
+                int value = V[j];
+
+                if (weight>i){
+                    dp[j][i] = dp[j-1][i];
+                }
+                else{
+                    dp[j][i] = Math.max(dp[j-1][i], dp[j-1][i-weight]+value);
+                }
+            }
         }
 
-        if (dp[i][K] == null){
-            if (W[i]>K){
-                dp[i][K] = fillBag(i-1, K);
-            }
-            else{
-                dp[i][K] = Math.max(fillBag(i-1, K), fillBag(i-1, K-W[i])+V[i]);
-            }
-        }
-
-        return dp[i][K];
+        return dp[N][K];
     }
 }//class
