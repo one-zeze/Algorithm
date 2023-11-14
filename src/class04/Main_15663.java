28e802b990fdea416000de491b80dbc1098946d8
one-zeze <tales0708@gmail.com>
2023-11-14 18:36:10 +0900

    not solved

---
+++ src/class04/Main_15663.java
@@ -0,0 +1,17 @@
+package class04;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_15663 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+
+        int N = Integer.parseInt(input[0]);
+        int M = Integer.parseInt(input[1]);
+
+
+    }
+}
