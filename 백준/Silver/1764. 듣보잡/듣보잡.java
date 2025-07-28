70a3d0b6f62af0185ae85b91cc6b4ce82937ab58
wonzae <83583278+one-zeze@users.noreply.github.com>
2025-07-28 15:07:24 +0900

    [Silver IV] Title: 듣보잡, Time: 244 ms, Memory: 26256 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -0,0 +1,41 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int N = Integer.parseInt(input[0]);
+        int M = Integer.parseInt(input[1]);
+
+        //듣도 못한
+        Map<String, Boolean> map = new HashMap<>();
+        for (int i=0; i<N; i++){
+            map.put(br.readLine(), false);
+        }
+
+        //보도 못한
+        ArrayList<String> result = new ArrayList<>();
+        for (int j=0; j<M; j++){
+            String name = br.readLine();
+            //듣보
+            if (map.containsKey(name)){
+                result.add(name);
+            }
+        }
+        //사전순 정렬
+        Collections.sort(result);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(result.size()).append("\n");
+        for (var item:result){
+            sb.append(item).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
