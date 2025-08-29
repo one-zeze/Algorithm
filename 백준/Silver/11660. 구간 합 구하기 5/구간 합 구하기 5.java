8dc50cc0ac2809db606c5eab48969b832aded865
wonzae <83583278+one-zeze@users.noreply.github.com>
2025-08-29 15:55:20 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 736 ms, Memory: 118776 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -3,25 +3,22 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-  static int[][] table;
-  static int[][] dp;
   public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st;
+    StringBuilder sb = new StringBuilder();
 
     String[] NM = br.readLine().split(" ");
     int N = Integer.parseInt(NM[0]); // N x N
     int M = Integer.parseInt(NM[1]); // result 개수
-    table = new int[N+1][N+1];
-    dp = new int[N+1][N+1];
+    int[][] dp = new int[N+1][N+1];
 
 
-    for (int col = 1; col <= N; col++) { // 표, dp 채우기
+    for (int row = 1; row <= N; row++) { // 표, dp 채우기
       st = new StringTokenizer(br.readLine());
-      for (int row = 1; row <= N; row++) {
+      for (int col = 1; col <= N; col++) {
         int value = Integer.parseInt(st.nextToken());
-        table[col][row] = value;
-        dp[col][row] = dp[col][row - 1] + dp[col - 1][row] - dp[col - 1][row - 1] + value;
+        dp[row][col] = dp[row][col - 1] + dp[row - 1][col] - dp[row - 1][col - 1] + value;
 
       }
     }
@@ -34,11 +31,11 @@ public class Main {
       int x2 = Integer.parseInt(st.nextToken());
       int y2 = Integer.parseInt(st.nextToken());
 
-      // System.out.println(getResult(x1, y1, x2, y2, table));
-      int result = dp[x2][y2] - dp[x2][y1-1] - dp[x1-1][y2] + dp[x1-1][y1-1];
-      System.out.println(result);
+      int result = dp[x2][y2] - dp[x2][y1 - 1] - dp[x1 - 1][y2] + dp[x1 - 1][y1 - 1];
+      sb.append(result).append("\n");
     }
 
+    System.out.print(sb.toString());
   }
   
 }
